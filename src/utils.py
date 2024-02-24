09aa658463dd2301040459b4de0ad01878d3579a
dirdam <adrian.jimenez@insightedge.jp>
2024-02-24 09:38:16 -0500

    Streamlit app

---
+++ src/utils.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def download_kaggle_dataset(dataset_name, path='data'):
+    """Download a Kaggle dataset to a specified path."""
+    from kaggle.api.kaggle_api_extended import KaggleApi
+    # No need to manually authenticate since they've been set as environment variables
+    api = KaggleApi()
+    api.authenticate()
+    api.dataset_download_files(dataset_name, path=path, unzip=True)
+
+def create_wins_cross_table(df):
+    """Create a cross table of the first hand color and the winning hand from a dataset of games played in BGA"""
+    # First hand colors
+    colors = {'y': 'Yellow', 'r': 'Red'}
+    df['first_hand_color'] = df['record'].apply(lambda x: colors[x[0]])
+    # First hand wins
+    df['winning_hand'] = df.apply(lambda row: 'First hand' if row['winner'] == row['first_hand'] else 'Second hand', axis=1)
+
+    # Cross table
+    cross_table = pd.crosstab(df['first_hand_color'], df['winning_hand'])
+    cross_table.index.name = None
+
+    # Add total sum row and column
+    cross_table['Total'] = cross_table.sum(axis=1)
+    cross_table.loc['Total'] = cross_table.sum()
+
+    return cross_table
+
+def table_to_percentage(table):
+    """Convert a table to percentage"""
+    return (table / table.loc['Total', 'Total'] * 100).applymap(lambda x: f"{x:.2f}%")
+
+def total_color(column):
+    color_total = 'rgba(150,150,150, 0.5)'
+    color_r = 'rgba(255,0,0, 0.25)'
+    color_y = 'rgba(255,255,0, 0.25)'
+    color_hand = 'rgba(200,200,200, 0.5)'
+    return ['background-color: %s' % (color_total if (column.name=='Total' and index=='Total') else (color_hand if (index=='Total') else (color_r if (column.name=='Total' and index=='Red') else (color_y if (column.name=='Total' and index=='Yellow') else '')))) for index, x in column.items()]
+
+def plot_hist(df, bin_width=1, title='Histogram'):
+    from scipy.stats import skewnorm
+    fig, ax = plt.subplots(1, 1)
+    # Histogram
+    bin_width = bin_width
+    bins = [i for i in range(df.min(), df.max(), bin_width)]
+    df.hist(bins=bins, edgecolor='white', grid=False, figsize=(20,8), alpha=0.5, ax=ax)
+    ax.set_title(title, fontsize=16, fontweight='bold')
+    # Distribution data
+    a, loc, scale = skewnorm.fit(df)
+    x = np.linspace(df.min(), df.max(), 100)
+    prop = len(df)*bin_width
+    ax.plot(x, prop*skewnorm.pdf(x, a, loc, scale), 'r-', lw=1, alpha=1, label=f'distribution approximation')
+    ax.axvline(x=df.mean(), color='g', label=f'mean: {df.mean():.2f}')
+    ax.axvline(x=df.median(), color='g', linestyle='--', label=f'median: {df.median():.2f}')
+    ax.legend()
+    ax.set_xlabel('Number of hands/moves per game', fontsize=16)
+    ax.set_ylabel('Number of games', fontsize=16)
+    return ax
+
+def code_to_old(code):
+    a = list(code)
+    for i in range(len(a) - 1):
+        if a[i] == 'y':
+            a[i] = 'b'
+        elif a[i] == 'r':
+            a[i] = 'l'
+            a[i+1] = str(6 - int(a[i+1]))
+    code = ''.join(a)
+    return code
---
+++ streamlit.py
@@ -0,0 +1,96 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import pandas as pd
+import os
+import seaborn as sns
+import matplotlib.pyplot as plt
+import src.utils as utils
+
+st.set_page_config(
+    page_title="Squadro",
+    page_icon="📈",
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items={
+        'Get help': "https://dirdam.github.io/contact.html",
+        'About': f"""This app was proudly developed by [Adrián Jiménez Pascual](https://dirdam.github.io/)."""
+    })
+
+
+# Set Kaggle environment variables
+os.environ["KAGGLE_USERNAME"] = st.secrets["kaggle_username"]
+os.environ["KAGGLE_KEY"] = st.secrets["kaggle_key"]
+
+# Specify the Kaggle dataset
+dataset_name = 'dirdam/squadro-games-played-in-bga' # Kaggle dataset URL
+download_path = 'data' # Local path where the dataset will be downloaded
+file_name = 'Squadro_BGA_history.csv' # Actual file name in the dataset
+
+# Download dataset
+utils.download_kaggle_dataset(dataset_name, download_path)
+
+# Load the dataset
+df = pd.read_csv(f'{download_path}/{file_name}')
+df['moves'] = df['record'].str.len()//2
+
+st.title('Squadro Statistics Visualizer')
+st.markdown('All the statistics shown here come from the latest version of the [Squadro games played in BGA](https://www.kaggle.com/datasets/dirdam/squadro-games-played-in-bga/data) dataset periodically uploaded to Kaggle.')
+st.markdown('As explained in the dataset description, the dataset contains information about most Squadro games played in [Board Game Arena](https://boardgamearena.com/) since it was made available on `14-12-2020`.')
+
+# Cross tables
+st.markdown('---')
+st.markdown('## Games won by hand and color')
+st.markdown('The tables show the _victories_ per _color_ and _hand_ combination.')
+threshold = st.slider('Restrict the tables values to games where both players had ELO greater than:', 0, 500, value=0, step=50)
+temp = df[(df['elo_winner'] >= threshold) & (df['elo_loser'] >= threshold)].copy()
+
+cross_table = utils.create_wins_cross_table(temp)
+percentage_table = utils.table_to_percentage(cross_table)
+# Show cross tables
+ct_col, per_col = st.columns(2)
+with ct_col:
+    st.markdown('### Total number')
+    st.write(cross_table.style.apply(utils.total_color).format("{:,.0f}"))
+with per_col:
+    st.write('### Percentage')
+    st.write(percentage_table.style.apply(utils.total_color))
+
+# Games length statistics
+st.markdown('## Games length histogram')
+st.markdown('''- **x-axis**: number of hands/moves (_plies_) per game.
+- **y-axis**: number of games.''')
+ax = utils.plot_hist(temp['moves'], bin_width=1, title='Squadro moves histogram')
+st.pyplot(ax.figure)
+
+# Replay
+st.markdown('---')
+st.markdown('## Replay specific game')
+df_replay_all = df[['date', 'winner', 'loser', 'elo_winner', 'elo_loser', 'moves']].copy()
+df_replay = df_replay_all.copy()
+cols = st.columns(3)
+with cols[0]:
+    date = st.date_input('Select date:', value=None, min_value=pd.to_datetime(df_replay['date'].min()), max_value=pd.to_datetime(df_replay['date'].max()))
+    if date: # If date is selected, filter any game played on that date, regardless the hour
+        df_replay = df_replay[pd.to_datetime(df_replay['date']).dt.date == date]
+with cols[1]:
+    players = df_replay[['winner', 'loser']].melt()['value'].value_counts().index
+    player1 = st.selectbox('Select player:', players, index=None, placeholder="Choose or type name")
+    if player1:
+        df_replay = df_replay[(df_replay['winner'] == player1) | (df_replay['loser'] == player1)]
+with cols[2]:
+    rivals_list = df_replay[['winner', 'loser']].melt()['value'].value_counts().index
+    player2 = st.selectbox('Select rival:', [r for r in rivals_list if r != player1], index=None, placeholder="Choose or type name", disabled=player1 is None)
+    if player2:
+        df_replay = df_replay[(df_replay['winner'] == player2) | (df_replay['loser'] == player2)]
+
+st.dataframe(df_replay)
+
+game_selection = st.selectbox('Select a game to visualize:', df_replay.index, index=None, placeholder="Choose or type row number", format_func=lambda x: f'{x} - "{df_replay.loc[x]["winner"]}" vs "{df_replay.loc[x]["loser"]}"')
+if game_selection:
+    print(df.loc[game_selection])
+    st.markdown(f'### Replay of _{df_replay.loc[game_selection]["winner"]}_ vs _{df_replay.loc[game_selection]["loser"]}_')
+    st.markdown('''- Click `▶️` to advance and `◀️` to go backwards. (`▶️▶️` and `◀️◀️` jump faster)
+- Click the top right corner of the board to see the game statistics for `Moves to finish`.''')
+    game_record = df.loc[game_selection]['record']
+    game_record = utils.code_to_old(game_record)
+    components.iframe(f'https://dirdam.github.io/games/squadro_replay.html?code={game_record}', height=600)
